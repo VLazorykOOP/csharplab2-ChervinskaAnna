31791a0c3f05e39c726443c094f40789dffb09b4
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2024-02-19 21:41:31 +0200

    task 3

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -67,13 +67,13 @@ namespace Lab2CSharp
             Console.WriteLine();
         }
         static void AdditionSingle(int[] a)
-        {  float b = 0;
+        { float b = 0;
             for (int i = 0; i < a.Length; ++i)
             {
                 b += a[i];
             }
             Console.WriteLine("Addition Single array: {0}", b);
-            if (b >= 10 && b <= 99)
+            if (b >= -99 && b <= -10 || b >= 10 && b <= 99)
             {
                 Console.WriteLine("It is double digit number");
             }
@@ -108,12 +108,12 @@ namespace Lab2CSharp
         {
             float b = 0;
             for (int i = 0; i < a.GetLength(0); ++i)
-                for (int j = 0; j < a.GetLength(1);  ++j)
+                for (int j = 0; j < a.GetLength(1); ++j)
                 {
-                   b+= a[i, j];
+                    b += a[i, j];
                 }
             Console.WriteLine("Addition Single array: {0}", b);
-            if (b >= 10 && b <= 99)
+            if (b >= -99 && b <= -10 || b >= 10 && b <= 99)
             {
                 Console.WriteLine("It is double digit number");
             }
@@ -146,46 +146,42 @@ namespace Lab2CSharp
             }
             else { Console.WriteLine("It doesn't exists:"); }
         }
-        //static int[] InputSingle()
-        //{
-        //    Console.WriteLine("Size of array: ");
-        //    int n = int.Parse(Console.ReadLine());
-        //    int[] a = new int[n];
-        //    for (int i = 0; i < n; ++i)
-        //    {
-        //        Console.Write("a[{0}]= ", i);
-        //        a[i] = int.Parse(Console.ReadLine());
-        //    }
-        //    return a;
-        //}
-        //static void PrintSingle(int[] a)
-        //{
-        //    for (int i = 0; i < a.Length; ++i) Console.Write("{0} ", a[i]);
-        //    Console.WriteLine();
-        //}
         static void CountSingle(int[] a)
         {
             int count = 0;
-            float prewA = 0;
-            for (int i = 0; i < a.Length; ++i)
+            float prewA = a[0];
+            for (int i = 1; i < a.Length; ++i)
             {
-                if (prewA > a[i]) {
+                if (prewA < a[i]) {
                     count++;
                 }
-                prewA= a[i];
+                prewA = a[i];
             }
             Console.WriteLine("Number whose > than next element: {0}", count);
-          
-        }
 
-        static void task2(){
+        }
+        static void task2() {
             Console.WriteLine("Task 2");
             int[] myArray = InputSingle();
             Console.WriteLine("Array:");
             PrintSingle(myArray);
             CountSingle(myArray);
         }
-
+        static void AverageMulti(int[,] a)
+        {
+            float sumLeftDiagonal = 0;
+            int countEL = 0;
+            for (int i = 0; i < a.GetLength(0); ++i) {
+                for (int j = 0; j < a.GetLength(0) - i; ++j){
+                    sumLeftDiagonal += a[i, j];
+                    countEL++;
+                }
+            }
+            float res = sumLeftDiagonal / countEL;
+            Console.WriteLine($"sumLeftDiagonal: {sumLeftDiagonal}");
+            Console.WriteLine($"countEL: {countEL}");
+            Console.WriteLine("Average value: {0}", res);
+        }
         static void task3()
         {
             Console.WriteLine("Task 3");
@@ -193,6 +189,7 @@ namespace Lab2CSharp
             int[,] myArray = InputMulti(out n, out m);
             Console.WriteLine("Array:");
             PrintMulti(myArray);
+            AverageMulti(myArray);
         }
         static void task4()
         {
